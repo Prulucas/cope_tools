a42cc6235d75a63275a0bb2ccedeaa311e7304e7
Prulucas <plbrandsilva@gmail.com>
2026-05-22 10:12:35 -0300

    Fix: adiciona CSRF_TRUSTED_ORIGINS e ajusta caminhos

--- cope_tools/settings.py
+++ cope_tools/settings.py
@@ -63,10 +63,11 @@ ROOT_URLCONF = 'cope_tools.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
