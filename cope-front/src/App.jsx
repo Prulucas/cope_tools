a2301828dd8104e10650b9396e52dad37ae9df19
Prulucas <plbrandsilva@gmail.com>
2026-05-24 22:37:29 -0300

    adding function lower in simple_tools and starting react to front

---
+++ cope-front/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>COPE Tools - Frontend React</h1>
+      <p>Pronto para conectar com o Django Rest Framework!</p>
+    </div>
+  )
+}
+
+export default App
